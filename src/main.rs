042d3c0dea9e6665c70c28c6335ff7ae63017156
petarpetrov <ipetrovbg@gmail.com>
2022-01-29 22:30:38 +0200

    feat: add some logging

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ use aws_sdk_sns::Client;
 use lambda_runtime::Context;
 use std::env;
 use std::error::Error;
+use dotenv::dotenv;
+use log::{debug, error, info};
 
 const URL_TO_PARSE: &str = "https://magazin.photosynthesis.bg/bg/64336-fotoaparat-sony-a7-iii.html?search_query=A7+III&results=21";
 
 #[tokio::main]
 async fn main() -> Result<(), lambda_runtime::Error> {
-    dotenv::dotenv().ok();
-
+    dotenv().ok();
+    debug!("Initializing lambda.");
     let handler = lambda_runtime::handler_fn(handler);
     lambda_runtime::run(handler).await?;
     Ok(())
 }
 
-async fn handler(_: (), _: Context) -> Result<(), lambda_runtime::Error> {
+async fn handler(_: String, _: Context) -> Result<bool, lambda_runtime::Error> {
     match make_request() {
         Ok(html_string) => {
+            info!("{}", env::var("TOPIC_ARN")?);
             let product = model::ParseProduct::new(html_string)
                 .parse_header()
                 .parse_price();
@@ -43,10 +46,10 @@ async fn handler(_: (), _: Context) -> Result<(), lambda_runtime::Error> {
                 .await?;
         }
         Err(_) => {
-            print("Couldn't make http request".to_string());
+            error!("Couldn't make http request");
         }
     };
-    Ok(())
+    Ok(true)
 }
 
 fn print(message: String) {
