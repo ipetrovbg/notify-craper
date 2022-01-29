e6c42a8516e22a4d6e6d392e19c02063ad8ef776
petarpetrov <ipetrovbg@gmail.com>
2022-01-29 21:43:23 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+mod model;
+use aws_sdk_sns::Client;
+use lambda_runtime::Context;
+use std::env;
+use std::error::Error;
+
+const URL_TO_PARSE: &str = "https://magazin.photosynthesis.bg/bg/64336-fotoaparat-sony-a7-iii.html?search_query=A7+III&results=21";
+
+#[tokio::main]
+async fn main() -> Result<(), lambda_runtime::Error> {
+    dotenv::dotenv().ok();
+
+    let handler = lambda_runtime::handler_fn(handler);
+    lambda_runtime::run(handler).await?;
+    Ok(())
+}
+
+async fn handler(_: (), _: Context) -> Result<(), lambda_runtime::Error> {
+    match make_request() {
+        Ok(html_string) => {
+            let product = model::ParseProduct::new(html_string)
+                .parse_header()
+                .parse_price();
+
+            let shared_config = aws_config::load_from_env().await;
+            let client = Client::new(&shared_config);
+            let topic_arn = env::var("TOPIC_ARN")?;
+
+            client
+                .publish()
+                .topic_arn(topic_arn)
+                .message(format!(
+                    "=============== Photosynthesis ===============\n\
+                    Requesting information for Sony A7 IIIn\n\
+                    Product: {}\n\
+                    Price: {}\n\
+                    {}
+                    ==============================================
+                    ",
+                    product.name, product.price, product.message
+                ))
+                .send()
+                .await?;
+        }
+        Err(_) => {
+            print("Couldn't make http request".to_string());
+        }
+    };
+    Ok(())
+}
+
+fn print(message: String) {
+    println!("> {}", message);
+}
+
+fn make_request() -> Result<String, Box<dyn Error>> {
+    let client = reqwest::blocking::Client::builder().build()?;
+    let response = client.get(URL_TO_PARSE).send()?;
+    let body_response = response.text()?;
+    Ok(body_response)
+}
---
+++ src/model.rs
@@ -0,0 +1,68 @@
+use scraper::{Html, Selector};
+const EXPENSIVE_PRICE: i32 = 3999;
+
+pub struct ParseProduct {
+    pub price: i32,
+    pub name: String,
+    pub message: String,
+    html: Option<Html>
+}
+
+impl ParseProduct {
+    pub fn new(html_string: String) -> Self {
+        ParseProduct {
+            html: Some(Html::parse_document(&html_string)),
+            name: "".to_string(),
+            price: 0,
+            message: "".to_string(),
+        }
+    }
+
+    pub fn parse_header(self) -> ParseProduct {
+        let html = self.html.unwrap();
+        let header_selector = Selector::parse(".header-item").unwrap();
+        let header_h1_selector = Selector::parse("h1").unwrap();
+        let header_element = html.select(&header_selector).next().unwrap();
+        let header_h1_element = header_element.select(&header_h1_selector).next().unwrap();
+        let header_text = header_h1_element.text().collect::<Vec<_>>();
+        let header = header_text.join("");
+
+        ParseProduct {
+            name: header.clone(),
+            price: self.price,
+            html: Some(html),
+            message: header,
+        }
+    }
+
+    pub fn parse_price(self) -> ParseProduct {
+        let html = self.html.unwrap();
+
+        let price_selector = Selector::parse("#our_price_display_64336").unwrap();
+        let price_el = html.select(&price_selector).next().unwrap();
+        let meta_selector = Selector::parse("meta").unwrap();
+        let price_meta_el = price_el.select(&meta_selector).next().unwrap();
+        let price_meta_value = price_meta_el.value().attrs();
+        let price_meta_vec = price_meta_value.collect::<Vec<_>>();
+        let mut product_price = 0;
+        let mut message = self.message;
+        for price in price_meta_vec.iter() {
+            if price.0.contains("content") {
+                let price_int = price.1.parse().unwrap_or(0);
+                product_price = price_int;
+                if price_int < EXPENSIVE_PRICE {
+                    message = format!("Go and buy 🎉");
+                } else {
+                    message = format!("Still too expensive 😕");
+                }
+
+            }
+        }
+        ParseProduct {
+            name: self.name,
+            html: Some(html),
+            price: product_price,
+            message
+        }
+    }
+}
